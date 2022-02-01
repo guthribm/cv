3f8884d07140c9dc76728ea262bcdc1b289e2110
Brandon Guthrie <guthribm@gmail.com>
2022-02-01 15:28:56 -0600

    adjusts buttons on projects

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 let date = document.getElementById("date");
+
+let body = document.body;
+body.addEventListener("scroll", function (event) {
+  console.log(`something happened. window.scrollY: ${document.window.scrollY}`);
+});
+
 let today = new Date();
 date.textContent = today.getFullYear();
+
+// const sections = document.querySelectorAll(".invisible");
+// const options = {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: "0px 0px -125px 0px",
+// };
+// const observer = new IntersectionObserver(function (entries, observer) {
+//   entries.forEach((entry) => {
+//     entry.target.classList.toggle("fade-in");
+//   });
+// }, options);
+
+// sections.forEach((section) => {
+//   observer.observe(section);
+// });
