ce9cffc05d1bad570e3847d845168e79b4c4a1db
Brandon Guthrie <83515736+guthribm@users.noreply.github.com>
2025-08-09 01:37:01 -0500

    save

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cv_react/", // Set base path for GitHub Pages
+  base: "/cv-react/", // Correct base path (repo name has hyphen)
 });
